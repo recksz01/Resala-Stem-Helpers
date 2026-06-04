3664a1ac9c8a4bb0bf4d0a66736126277797a98b
recksz01 <omarafify12233344om@gmail.com>
2026-06-05 01:09:54 +0300

    Fix: Remove missing STEMCompanion import that was causing build failure

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollProgress from "./components/ScrollProgress";
 import LoadingScreen from "./components/LoadingScreen";
-import STEMCompanion from "./components/STEMCompanion"; // 👈 استيراد العنكبوت الجديد هنا
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Programs from "./pages/Programs";
@@ -31,7 +30,6 @@ export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <STEMCompanion /> {/* 👈 إضافة المكون في جذع الشجرة ليعمل في كافة الصفحات */}
         <LoadingScreen />
         <ScrollToTop />
         <ScrollProgress />
